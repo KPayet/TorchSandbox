0c105571bd8f5772a7381a8785a6d411fad32603
Ubuntu <ubuntu@ip-172-31-40-57.eu-west-1.compute.internal>
2015-04-21 16:46:38 +0000

    added script for feedforward net

---
+++ mnist/h2o_ffnn.lua
@@ -0,0 +1,7 @@
+-- Simple supervised learning on MNIST, using a feedforward neural net (brute forcing)
+-- Uses the h2o training version of mnist
+-- This is a one file script, no options, just to keep it simple
+
+-- Loading data
+
+
