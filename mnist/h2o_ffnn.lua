abe53d2a546077ab11989e87df9c9308c0791e00
Kevin Payet <payet@payetVM.vmmachine>
2015-04-21 20:02:29 +0200

    modified h2o_ffnn.lua

--- mnist/h2o_ffnn.lua
+++ mnist/h2o_ffnn.lua
@@ -8,6 +8,7 @@ require 'os'
 require 'torch'
 require 'nn'
 require 'csvigo'
+require 'optim'
 
 local data_path = "./data/"
 local train_file = "h2o_train.csv"
@@ -77,4 +78,121 @@ model:add(nn.LogSoftMax()) -- needed for NLL criterion
 
 criterion = nn.ClassNLLCriterion()
 
+-- Training -- This part is an almost copy/paste of http://code.madbits.com/wiki/doku.php?id=tutorial_supervised_4_train
+
+classes = {'1','2','3','4','5','6','7','8','9','0'}
+
+-- This matrix records the current confusion across classes
+confusion = optim.ConfusionMatrix(classes)
+
+-- Log results to files
+trainLogger = optim.Logger(paths.concat('./train.log'))
+testLogger = optim.Logger(paths.concat('./test.log'))
+
+if model then
+    parameters,gradParameters = model:getParameters()
+end
+
+trsize = trainData.data.size()
+batchSize = 1
+
+-- Training function
+function train()
+
+   -- epoch tracker
+   epoch = epoch or 1
+
+   -- local vars
+   local time = sys.clock()
+
+   -- shuffle at each epoch
+   shuffle = torch.randperm(trsize)
+
+   -- do one epoch
+   print('==> doing epoch on training data:')
+   print("==> online epoch # " .. epoch)
+   for t = 1,trainData:size(),batchSize do
+      -- disp progress
+      xlua.progress(t, trainData:size())
+
+      -- create mini batch
+      local inputs = {}
+      local targets = {}
+      for i = t,math.min(t+batchSize-1,trainData:size()) do
+	 -- load new sample
+	 local input = trainData.data[shuffle[i]]:double()
+	 local target = trainData.labels[shuffle[i]]
+	 table.insert(inputs, input)
+	 table.insert(targets, target)
+      end
+
+      -- create closure to evaluate f(X) and df/dX
+      local feval = function(x)
+		       -- get new parameters
+		       if x ~= parameters then
+			  parameters:copy(x)
+		       end
+
+		       -- reset gradients
+		       gradParameters:zero()
+
+		       -- f is the average of all criterions
+		       local f = 0
+
+		       -- evaluate function for complete mini batch
+		       for i = 1,#inputs do
+			  -- estimate f
+			  local output = model:forward(inputs[i])
+			  local err = criterion:forward(output, targets[i])
+			  f = f + err
+
+			  -- estimate df/dW
+			  local df_do = criterion:backward(output, targets[i])
+			  model:backward(inputs[i], df_do)
+
+			  -- update confusion
+			  confusion:add(output, targets[i])
+		       end
+
+		       -- normalize gradients and f(X)
+		       gradParameters:div(#inputs)
+		       f = f/#inputs
+
+		       -- return f and df/dX
+		       return f,gradParameters
+		    end
+
+	config = config or {learningRate = 1e-3,
+			 weightDecay = 0,
+			 momentum = 0,
+			 learningRateDecay = 5e-7}
+	optim.sgd(feval, parameters, config)
+
+   end
+
+   -- time taken
+   time = sys.clock() - time
+   time = time / trainData:size()
+   print("==> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
+   -- print confusion matrix
+   print(confusion)
+   confusion:zero()
+
+   -- update logger/plot
+   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
+   if opt.plot then
+      trainLogger:style{['% mean class accuracy (train set)'] = '-'}
+      trainLogger:plot()
+   end
+
+   -- save/log current net
+   local filename = paths.concat('./model_'..epoch..'.net')
+   os.execute('mkdir -p ' .. sys.dirname(filename))
+   print('==> saving model to '..filename)
+   torch.save(filename, model)
+
+   -- next epoch
+   epoch = epoch + 1
+end
 
