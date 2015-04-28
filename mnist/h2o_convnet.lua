c6dc21e4de12716e35565fa079ec0b1c95de55b9
Kevin Payet <payet@payetVM.vmmachine>
2015-04-28 23:17:29 +0200

    modifs to h2o_convnet.lua

--- mnist/h2o_convnet.lua
+++ mnist/h2o_convnet.lua
@@ -100,7 +100,7 @@ model:add(nn.LogSoftMax()) -- needed for NLL criterion
 --Loss function
 
 criterion = nn.ClassNLLCriterion()
---criterion:cuda()
+criterion:cuda()
 
 -- Training -- This part is an almost copy/paste of http://code.madbits.com/wiki/doku.php?id=tutorial_supervised_4_train
 
@@ -120,156 +120,8 @@ end
 trsize = trainData:size()
 batchSize = 1
 
--- Training function for cuda
+-- Training function
 function train(maxEntries)
-   
-   local maxEntries = maxEntries or trainData:size()   
-
-   -- epoch tracker
-   epoch = epoch or 1
-
-   -- local vars
-   local time = sys.clock()
-   
-   model:training()
-    
-   -- shuffle at each epoch
-   shuffle = torch.randperm(maxEntries)
-
-   -- do one epoch
-   print('==> doing epoch on training data:')
-   print("==> online epoch # " .. epoch)
-   for t = 1,maxEntries,batchSize do
-      -- disp progress
-      xlua.progress(t, maxEntries)
-
-      -- create mini batch
-      local inputs = {}
-      local targets = {}
-      for i = t,math.min(t+batchSize-1,maxEntries) do
-	 -- load new sample
-	 local input = trainData.data[shuffle[i]]:double()
-	 local target = trainData.labels[shuffle[i]]
-	 table.insert(inputs, input)
-	 table.insert(targets, target)
-      end
-	
-      -- create closure to evaluate f(X) and df/dX
-      local feval = function(x)
-		       -- get new parameters
-		       if x ~= parameters then
-			  parameters:copy(x)
-		       end
-
-		       -- reset gradients
-		       gradParameters:zero()
-
-		       -- f is the average of all criterions
-		       local f = 0
-			
-		       -- evaluate function for complete mini batch
-		       for i = 1,#inputs do
-			  -- estimate f
-			  local output = model:forward(inputs[i])
-			  if targets[i]==0 then targets[i]=10 end
-			  
-			  local err = criterion:forward(output, targets[i])
-			  f = f + err
-			  
-			  -- estimate df/dW
-			  local df_do = criterion:backward(output, targets[i])
-			  model:backward(inputs[i], df_do)
-
-			  -- update confusion
-			  confusion:add(output, targets[i])
-		       end
-
-		       -- normalize gradients and f(X)
-		       gradParameters:div(#inputs)
-		       f = f/#inputs
-
-		       -- return f and df/dX
-		       return f,gradParameters
-		    end
-
-	config = config or {learningRate = 1e-3,
-			 weightDecay = 0,
-			 momentum = 0,
-			 learningRateDecay = 5e-7}
-	
-	optim.sgd(feval, parameters, config)
-	
-
-   end
-
-   -- time taken
-   time = sys.clock() - time
-   time = time / maxEntries
-   print("==> time to learn 1 sample = " .. (time*1000) .. 'ms')
-
-   -- print confusion matrix
-   print(confusion)
-   confusion:zero()
-
-   -- update logger/plot
-   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
-
-   -- save/log current net
-   local filename = paths.concat('./h2o_ffnn_model_'..epoch..'.net')
-   
-   print('==> saving model to '..filename)
-  -- torch.save(filename, model)
-
-   -- next epoch
-   epoch = epoch + 1
-end
-
-function test(maxEntries)
-   
-   local maxEntries = maxEntries or testData:size()
-   
-   -- local vars
-   local time = sys.clock()
-
-   -- averaged param use?
-   if average then
-      cachedparams = parameters:clone()
-      parameters:copy(average)
-   end
-   
-   model:evaluate()
-
-   -- test over test data
-   print('==> testing on test set:')
-   for t = 1,maxEntries do
-      -- disp progress
-      xlua.progress(t, maxEntries)
-
-      -- get new sample
-      local input = testData.data[t]:double()
-      local target = testData.labels[t]
-      if target == 0 then target = 10 end
-
-      -- test sample
-      local pred = model:forward(input)
-      confusion:add(pred, target)
-   end
-
-   -- timing
-   time = sys.clock() - time
-   time = time / maxEntries
-   print("==> time to test 1 sample = " .. (time*1000) .. 'ms')
-
-   -- print confusion matrix
-   print(confusion)
-   confusion:zero()
-
-   -- update log/plot
-   testLogger:add{['% mean class accuracy (test set)'] = confusion.totalValid * 100}
-end
-
--- Training function for cuda
-function train_cuda(maxEntries)
    
    local maxEntries = maxEntries or trainData:size()   
 
@@ -373,7 +225,7 @@ function train_cuda(maxEntries)
    epoch = epoch + 1
 end
 
-function test_cuda(maxEntries)
+function test(maxEntries)
    
    local maxEntries = maxEntries or testData:size()
    
@@ -418,11 +270,9 @@ function test_cuda(maxEntries)
    testLogger:add{['% mean class accuracy (test set)'] = confusion.totalValid * 100}
 end
 
-
---[[
-epoch=1
+--[[epoch=1
 while epoch<11 do
    train()
    test()
-end--]]--
+end--]]
 
